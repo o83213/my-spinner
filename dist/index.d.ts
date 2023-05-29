9a190a941647c4ee944e9c27741532abd9c4d33d
LIONMAIL\brianjiang <brianjiang@liontravel.com>
2023-05-29 11:46:23 +0800

    first commit

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import * as react_jsx_runtime from 'react/jsx-runtime';
+
+declare function LoadingSpinner(): react_jsx_runtime.JSX.Element;
+
+export { LoadingSpinner as Spinner };
---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import json from "@rollup/plugin-json";
+// To handle css files
+// import postcss from "rollup-plugin-postcss";
+import terser from "@rollup/plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/cjs/index.js",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/esm/index.js",
+        format: "es",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      json(),
+      // postcss(),
+      terser(),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts.default()],
+    external: [/\.css$/], // telling rollup anything that is .css aren't part of type exports
+  },
+];
